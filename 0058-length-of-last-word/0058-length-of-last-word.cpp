4ecf7da13e4440e340f24ec77e90296ba7a7095d
itzsneha-prog <sneha67kumari@gmail.com>
2026-07-30 11:49:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (35.14%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        // int n=s.size();
+        // int m=s.size()-1;
+        // while(m>=0 && s[m]==' ' ){
+        //     s.erase(m,1);
+        //     m--;
+        // }
+        // m=s.size()-1;
+        // int count=0;
+        // while(m>=0 && s[m]!=' '){
+        //     count++;
+        //     m--;
+        // }
+        // return count;
+
+        //more optimal approch
         int n=s.size();
-        int m=s.size()-1;
-        while(m>=0 && s[m]==' ' ){
-            s.erase(m,1);
+        int m=n-1;
+        int count=0;
+        while(m>0 && s[m] == ' '){
             m--;
         }
-        m=s.size()-1;
-        int count=0;
+        if(m<0){
+            return -1;
+        }else{
         while(m>=0 && s[m]!=' '){
             count++;
             m--;
         }
         return count;
+        }
     }
 };
