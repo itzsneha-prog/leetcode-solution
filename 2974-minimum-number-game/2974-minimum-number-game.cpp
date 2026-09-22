eb03d98c66221a85076c5d9470548f9a6c75a9e9
itzsneha-prog <sneha67kumari@gmail.com>
2026-09-23 00:02:14 +0530

    Time: 0 ms (100%), Space: 28.1 MB (77.47%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.cpp
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -3,6 +3,27 @@ class Solution {
 public:
 
     vector<int> numberGame(vector<int>& nums) {
+        vector<int>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i+=2){
+            swap(nums[i],nums[i+1]);
+        }
+
+        return nums;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         sort(nums.begin(), nums.end());
 
